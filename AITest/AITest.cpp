f94e550a1ecf6ed736927fc54f634f7ccf3d9d82
sohnryang <loop.infinitely@gmail.com>
2019-05-16 07:25:30 +0900

    added test project

---
+++ AITest/AITest.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace AITest {
+	TEST_CLASS(AITest) {
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
