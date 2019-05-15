6c916c8815a15c5ca2659f2473b02ad686efe57e
sohnryang <loop.infinitely@gmail.com>
2019-05-16 02:11:51 +0900

    deleted newlines

--- GomokuAI/board.h
+++ GomokuAI/board.h
@@ -8,10 +8,7 @@ private:
     vector<vector<int>> board_v;
 public:
     Board();
-
     void place_stone(int y, int x, int type);
-
     int check_pos(int y, int x);
-
     vector<vector<int>> raw_data();
 };
