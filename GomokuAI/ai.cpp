1e2028ad98f12fbc3948a42e1a95a5b95451f529
sohnryang <loop.infinitely@gmail.com>
2019-05-16 12:21:13 +0900

    added incomplete AI
    Not for use.

--- GomokuAI/ai.cpp
+++ GomokuAI/ai.cpp
@@ -1,6 +1,21 @@
 #include "ai.h"
 #include "global.h"
 
+vector<pair<int, int>> ai::next_moves(Board board) {
+    vector<pair<int, int>> result;
+    for (int i = 0; i < 19; ++i) {
+        for (int j = 0; j < 19; ++j) {
+            if (board.check_pos(i, j) != 0) continue;
+            bool legit = false;
+            for (int k = 0; k < 8; ++k)
+                if (board.check_pos(i + dy[k], j + dx[k]) != 0)
+                    legit = true;
+            if (legit) result.push_back(make_pair(i, j));
+        }
+    }
+    return result;
+}
+
 int ai::evaluate_board(Board board) {
     if (!board.game_running().first)
         return board.game_running().second == 2 ? INF : -INF;
--- GomokuAI/ai.h
+++ GomokuAI/ai.h
@@ -2,6 +2,7 @@
 #include "board.h"
 
 namespace ai {
+    vector<pair<int, int>> next_moves(Board board);
     int evaluate_board(Board board);
     pair<int, pair<int, int>> run_ai(Board board, bool ai_turn, int depth,
                                      int alpha, int beta);
