6cb46c1360da19eb0e45f0ff09dd1fa48e98cd24
sohnryang <loop.infinitely@gmail.com>
2019-05-16 13:42:46 +0900

    added malfunctioning logic

--- GomokuAI/ai.cpp
+++ GomokuAI/ai.cpp
@@ -1,5 +1,9 @@
+#include <algorithm>
 #include "ai.h"
 #include "global.h"
+
+using std::max;
+using std::min;
 
 vector<pair<int, int>> ai::next_moves(Board board) {
     vector<pair<int, int>> result;
@@ -24,11 +28,33 @@ int ai::evaluate_board(Board board) {
 
 pair<int, pair<int, int>> ai::run_ai(Board board, bool ai_turn, int depth,
                                      int alpha, int beta) {
-    pair<int, int> best = make_pair(-1, -1);
+    pair<int, int> best_move = make_pair(-1, -1);
     int best_score = ai_turn ? -INF : INF;
     if (!board.game_running().first) {
         best_score = evaluate_board(board);
-        return make_pair(best_score, best);
+        return make_pair(best_score, best_move);
     }
-    return pair<int, pair<int, int>>();
+    vector<pair<int, int>> legal_moves = next_moves(board);
+    for (auto move: legal_moves) {
+        Board board_copy = board;
+        board_copy.place_stone(move.first, move.second, ai_turn ? 2 : 1);
+        if (ai_turn) {
+            int score = run_ai(board_copy, false, depth + 1, alpha, beta).first;
+            if (best_score < score) {
+                best_score = score - depth * 10;
+                best_move = move;
+                alpha = max(alpha, best_score);
+                if (beta <= alpha) break;
+            }
+        } else {
+            int score = run_ai(board_copy, true, depth + 1, alpha, beta).first;
+            if (best_score > score) {
+                best_score = score + depth * 10;
+                best_move = move;
+                beta = min(beta, best_score);
+                if (beta <= alpha) break;
+            }
+        }
+    }
+    return make_pair(best_score, best_move);
 }
