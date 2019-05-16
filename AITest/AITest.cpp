60497ac99c86deabe66f0aa866079aba4a152c56
sohnryang <loop.infinitely@gmail.com>
2019-05-16 13:19:16 +0900

    fixed failing test

--- AITest/AITest.cpp
+++ AITest/AITest.cpp
@@ -32,27 +32,27 @@ namespace AITest {
             vector<int> empty_line(19, 0);
             vector<vector<int>> empty_grid(19, empty_line);
             Board b1(empty_grid);
-            Assert::IsTrue(b1.game_running());
+            Assert::IsTrue(b1.game_running().first);
             vector<vector<int>> straight_h(19, empty_line);
             for (int i = 0; i < 5; ++i) straight_h[0][i] = 1;
             Board b2(straight_h);
-            Assert::IsFalse(b2.game_running());
+            Assert::IsFalse(b2.game_running().first);
             vector<vector<int>> straight_v(19, empty_line);
             for (int i = 0; i < 5; ++i) straight_v[i][0] = 1;
             Board b3(straight_v);
-            Assert::IsFalse(b3.game_running());
+            Assert::IsFalse(b3.game_running().first);
             vector<vector<int>> diag_tl_br(19, empty_line);
             for (int i = 0; i < 5; ++i) diag_tl_br[i][i] = 1;
             Board b4(diag_tl_br);
-            Assert::IsFalse(b4.game_running());
+            Assert::IsFalse(b4.game_running().first);
             vector<vector<int>> diag_tr_bl(19, empty_line);
             for (int i = 0; i < 5; ++i) diag_tr_bl[i][4 - i] = 1;
             Board b5(diag_tr_bl);
-            Assert::IsFalse(b5.game_running());
+            Assert::IsFalse(b5.game_running().first);
             vector<int> full_line(19, 1);
             vector<vector<int>> full_grid(19, full_line);
             Board b6(full_grid);
-            Assert::IsFalse(b6.game_running());
+            Assert::IsFalse(b6.game_running().first);
         }
 	};
 }
